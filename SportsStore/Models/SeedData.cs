d8d2a9860f65df82c17ca9d3069b3d64f867bbde
Иван Завадский <zavadskiy1998@yandex.ru>
2024-04-20 12:47:34 +0400

    Добавьте файлы проекта.

---
+++ SportsStore/Models/SeedData.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SportsStore.Models
+{
+    public static class SeedData
+    {
+        public static void EnsurePopulated (IApplicationBuilder app)
+        {
+            ApplicationDbContext context = app.ApplicationServices.GetRequiredService<ApplicationDbContext>();
+            context.Database.Migrate();
+            if(!context.Products.Any())
+            {
+                context.Products.AddRange(
+                    new Product
+                    {
+                        Name = "Kayak",
+                        Description = "A board for one person",
+                        Category = "Watersports",
+                        Price = 275
+                    },
+                    new Product
+                    {
+                        Name = "Lifeajcket",
+                        Description = "Protective and fashionable",
+                        Category = "Watersports",
+                        Price = 48.95m
+                    }, 
+                    new Product
+                    {
+                        Name = "Soccer Ball",
+                        Description = "Fifa-approved size and weight",
+                        Category = "Soccer",
+                        Price = 19.50m
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
