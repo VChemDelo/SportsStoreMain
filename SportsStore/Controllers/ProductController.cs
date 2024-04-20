2237dfdf169590c52dd378e0263250506838e532
Иван Завадский <zavadskiy1998@yandex.ru>
2024-04-20 22:48:19 +0400

    =

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -1,15 +1,17 @@
 ﻿using SportsStore.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace SportsStore.Controllers
 {
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 2;
         public ProductController(IProductRepository repo) 
         {
             repository = repo;
         }
-        public ViewResult List() => View (repository.Products);
+        public ViewResult List(int productPage = 1) => View(repository.Products.OrderBy(p => p.ProductID).Skip((productPage - 1) * PageSize).Take(PageSize));
     }
 }
